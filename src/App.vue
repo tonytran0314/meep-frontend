e63df1fa8ee31e7292fbf12e1eb2bad5918d1abe
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-13 16:37:55 -0500

    install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue'
-
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
@@ -17,37 +14,25 @@ window.Echo = new Echo({
     enabledTransports: ['ws', 'wss'],
 });
 
-onMounted(() => {
-  console.log('ready')
-  window.Echo.channel('public-chat').listen('.SendMessage', (event) => {
-    console.log('Message Received: ' + event.message)
-  })
+window.Echo.channel('public-chat').listen('.SendMessage', (event) => {
+  console.log('Message Received: ' + event.message)
 })
+
+const sendMessage = () => {
+  console.log('ok sent')
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <form @submit.prevent="sendMessage" class="max-w-sm mx-auto">
+    <div class="mb-5">
+      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+      <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+    </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  </form>
+  
+  <div class="bg-red-300">
+    <p>asdsad</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
