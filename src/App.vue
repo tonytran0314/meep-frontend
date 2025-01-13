39a3fc2a4452f0c3a704c2639ac9134eb30bab1f
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-13 17:45:16 -0500

    install and setup axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { api } from '@/services/axios.js'
+import { ref } from 'vue';
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
+
+const latestMessage = ref(null)
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
@@ -15,11 +19,17 @@ window.Echo = new Echo({
 });
 
 window.Echo.channel('public-chat').listen('.SendMessage', (event) => {
+  latestMessage.value = event.message
   console.log('Message Received: ' + event.message)
 })
 
-const sendMessage = () => {
-  console.log('ok sent')
+const messageContent = ref(null)
+const sendMessage = async () => {
+  try {
+    const res = await api.post('/send-message', { content: messageContent.value })
+  } catch (error) {
+    console.log(error)
+  }
 }
 </script>
 
@@ -27,12 +37,12 @@ const sendMessage = () => {
   <form @submit.prevent="sendMessage" class="max-w-sm mx-auto">
     <div class="mb-5">
       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-      <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+      <input v-model="messageContent" type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
     </div>
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
   </form>
   
   <div class="bg-red-300">
-    <p>asdsad</p>
+    <p>Latest message: {{ latestMessage }}</p>
   </div>
 </template>
---
+++ src/services/axios.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+// import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
+
+// NProgress.configure({
+//   showSpinner: false,
+//   trickleSpeed: 300
+// });
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  withXSRFToken: true
+})
+
+// api.interceptors.request.use((config) => {
+//   NProgress.start();
+//   return config;
+// });
+
+
+// api.interceptors.response.use(
+//   (response) => {
+//     NProgress.done();
+//     return response;
+//   },
+//   (error) => {
+//     NProgress.done();
+//     return Promise.reject(error);
+//   }
+// );
+
+
+export const csrf = axios.create({
+  baseURL: import.meta.env.VITE_CSRF_URL,
+  withCredentials: true,
+  withXSRFToken: true
+})
