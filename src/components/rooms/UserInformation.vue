a128a051627277552264ae3b575bc8831c681556
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-15 22:35:37 -0500

    split rooms container to components

---
+++ src/components/rooms/UserInformation.vue
@@ -0,0 +1,18 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div class="flex gap-2 p-3 rounded-lg cursor-pointer hover:bg-gray-200">
+        <!-- AVATAR -->
+        <div class="size-12 rounded-full bg-teal-600 shadow-lg"></div>
+        <!-- FULLNAME -->
+        <div>
+            <p class="text-lg">Trần Gia Huy</p>
+            <div class="flex items-center gap-2">
+                <p class="text-gray-400">Online status</p>
+                <div class="size-3 rounded-full bg-green-400"></div>
+            </div>
+        </div>
+    </div>
+</template>
