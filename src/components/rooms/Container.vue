0d6302effec4c8d0b22ed0d589cc7ee3e0eb8034
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-15 23:25:52 -0500

    feat: get profile name

--- src/components/rooms/Container.vue
+++ src/components/rooms/Container.vue
@@ -1,11 +1,20 @@
 <script setup>
     import UserInformation from '@/components/rooms/UserInformation.vue'
     import RoomList from '@/components/rooms/List.vue'
+    import { authStore } from '@/stores/authentication'
+
+    const auth = authStore()
 </script>
 
 <template>
     <div class="size-20 bg-white dark:bg-gray-800 shadow-lg rounded-2xl h-full w-96 p-4 flex flex-col gap-8">
-        <UserInformation />
+        <Suspense>
+            <UserInformation />
+            <template #fallback>
+                Loading user info...
+            </template>
+        </Suspense>
+        <button @click="auth.logout" class="bg-teal-200">Logout</button>
         <RoomList />
     </div>
 </template>
--- src/components/rooms/UserInformation.vue
+++ src/components/rooms/UserInformation.vue
@@ -1,5 +1,9 @@
 <script setup>
-    
+    import { profileStore } from '@/stores/profile'
+
+    const profile = profileStore()
+
+    await profile.get()
 </script>
 
 <template>
@@ -8,7 +12,7 @@
         <div class="size-12 rounded-full bg-teal-600 shadow-lg"></div>
         <!-- FULLNAME -->
         <div>
-            <p class="text-lg">Trần Gia Huy</p>
+            <p class="text-lg">{{ profile.name }}</p> 
             <div class="flex items-center gap-2">
                 <p class="text-gray-400">Online status</p>
                 <div class="size-3 rounded-full bg-green-400"></div>
