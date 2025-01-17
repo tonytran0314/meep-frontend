326971c8e5f24045dcac13460a2d50c0bf0d7ea8
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-17 01:52:20 -0500

    feat: back button for menu

--- src/components/modals/LogoutModal.vue
+++ src/components/modals/LogoutModal.vue
@@ -1,12 +1,13 @@
 <script setup>
     import { modalStore } from '@/stores/modal'
     import { authStore } from '@/stores/authentication'
+    import MenuModal from '@/components/modals/MenuModal.vue'
 
     const modal = modalStore()
     const auth = authStore()
 
-    const cancel = () => {
-        modal.close()
+    const back = () => {
+        modal.open(MenuModal, { title: 'Menu' })
     }
 
     const logout = async () => {
@@ -19,7 +20,7 @@
     <div class="space-y-8">
         <div>Are you sure you want to log out?</div>
         <div class="flex justify-end gap-4 text-sm text-white">
-            <div @click="cancel" class="bg-gray-500 rounded-lg px-6 py-2 cursor-pointer hover:bg-gray-600">Cancel</div>
+            <div @click="back" class="bg-gray-500 rounded-lg px-6 py-2 cursor-pointer hover:bg-gray-600">Back</div>
             <div @click="logout" class="bg-teal-600 rounded-lg px-6 py-2 cursor-pointer hover:bg-teal-500">Logout</div>
         </div>
     </div>
--- src/components/modals/ProfileModal.vue
+++ src/components/modals/ProfileModal.vue
@@ -1,7 +1,16 @@
 <script setup>
-
+    import { modalStore } from '@/stores/modal'
+    import MenuModal from '@/components/modals/MenuModal.vue'
+    
+    const modal = modalStore()
+    
+    const back = () => {
+        modal.open(MenuModal, { title: 'Menu' })
+    }
 </script>
 
 <template>
     <div>profile modal</div>
+    
+    <div @click="back" class="bg-gray-500 rounded-lg px-6 py-2 cursor-pointer hover:bg-gray-600 text-white">Back</div>
 </template>
