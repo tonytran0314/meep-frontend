e96261c153b6db77fdc25fe15e05463ad57e8408
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-17 02:01:17 -0500

    style: theme section in settings

--- src/components/modals/SettingsModal.vue
+++ src/components/modals/SettingsModal.vue
@@ -10,6 +10,18 @@
 </script>
 
 <template>
-    <div>settings modal</div>
+    <div class="bg-red-300">
+        <div class="flex justify-between">
+            <p>Theme</p>
+            <!-- <select v-model="selectedTheme" @change="updateThemeMode" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-600 dark:focus:border-emerald-600">
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select> -->
+            <select>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+            </select>
+        </div>
+    </div>
     <div @click="back" class="bg-gray-500 rounded-lg px-6 py-2 cursor-pointer hover:bg-gray-600 text-white">Back</div>
 </template>
