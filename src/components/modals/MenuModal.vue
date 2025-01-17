0f4fe4f2197ac0602d6dea7434fddd7748889298
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-17 01:47:37 -0500

    create settings and profile modal

--- src/components/modals/MenuModal.vue
+++ src/components/modals/MenuModal.vue
@@ -1,9 +1,19 @@
 <script setup>
     import { modalStore } from '@/stores/modal'
+    import ProfileModal from '@/components/modals/ProfileModal.vue'
+    import SettingsModal from '@/components/modals/SettingsModal.vue'
     import LogoutModal from '@/components/modals/LogoutModal.vue'
     
     const modal = modalStore()
 
+    const openProfile = () => {
+        modal.open(ProfileModal, { title: 'Profile' })
+    }
+
+    const openSettings = () => {
+        modal.open(SettingsModal, { title: 'Settings' })
+    }
+    
     const confirmLogout = () => {
         modal.open(LogoutModal, { title: 'Logout' })
     }
@@ -11,11 +21,11 @@
 
 <template>
     <div class="space-y-1">
-        <div @click="confirmLogout" class="hover:bg-gray-200 rounded-lg cursor-pointer flex items-center gap-2 px-4 py-3">
+        <div @click="openProfile" class="hover:bg-gray-200 rounded-lg cursor-pointer flex items-center gap-2 px-4 py-3">
             <font-awesome-icon :icon="['fas', 'user']" />
             <p>Profile</p>
         </div>
-        <div @click="confirmLogout" class="hover:bg-gray-200 rounded-lg cursor-pointer flex items-center gap-2 px-4 py-3">
+        <div @click="openSettings" class="hover:bg-gray-200 rounded-lg cursor-pointer flex items-center gap-2 px-4 py-3">
             <font-awesome-icon :icon="['fas', 'gear']" />
             <p>Settings</p>
         </div>
---
+++ src/components/modals/SettingsModal.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>settings modal</div>
+</template>
