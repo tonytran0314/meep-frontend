11a36f5c6996a6b7081cc322a5d885dfe741f1c0
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-17 03:20:12 -0500

    fix: light mode color for search bar

--- src/components/rooms/SearchBar.vue
+++ src/components/rooms/SearchBar.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <div class="bg-gray-700 px-4 py-2 rounded-2xl flex gap-3 items-center">   
-        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-        <input type="text" class="bg-gray-700 flex-1 focus:outline-none" placeholder="Search friends ...">
+    <div class="bg-white dark:bg-gray-700 border-2 dark:border-none px-4 py-2 rounded-full flex gap-3 items-center">   
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-gray-400" />
+        <input type="text" class="bg-white dark:bg-gray-700 flex-1 focus:outline-none" placeholder="Search friends ...">
     </div>
 </template>
