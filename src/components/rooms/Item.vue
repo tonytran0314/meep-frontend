252672b70b06dc3c5449ea7b06d9252f3daf0553
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-22 00:25:14 -0500

    feat: show preview message

---
+++ src/components/rooms/Item.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const props = defineProps({
+        roomId: Number,
+        latestMessage: String
+    })
+
+    const previewMessage = ref(props.latestMessage)
+
+    window.Echo.private(`room.${props.roomId}`)
+        .listen('.SendMessage', (event) => {
+            if(event.message.room_id === props.roomId) {
+                previewMessage.value = event.message.content
+            }
+        })
+</script>
+
+<template>
+    <div class="flex gap-2 p-3 rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700">
+        <!-- AVATAR -->
+        <div class="size-12 rounded-full bg-gray-500 shadow-lg"></div>
+        <!-- FULLNAME -->
+        <div>
+            <p>Room ID: {{ roomId }}</p>
+            <div class="flex items-center gap-2">
+                <p class="text-gray-400">{{ previewMessage }}</p>
+                <!-- <p class="text-gray-400">Preview mes... 00:00</p> -->
+                <div class="size-3 rounded-full bg-green-400"></div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/rooms/List.vue
+++ src/components/rooms/List.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import Room from '@/components/rooms/Item.vue'
+
     import { roomStore } from '@/stores/room.js'
 
     const room = roomStore()
@@ -14,18 +16,7 @@
     <div class="flex-1 overflow-auto space-y-4">
         <p class="text-lg text-gray-400">Conversations</p>
         <div>
-            <div v-for="room in room.list" @click="openRoom(room.id)" class="flex gap-2 p-3 rounded-lg cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700">
-                <!-- AVATAR -->
-                <div class="size-12 rounded-full bg-gray-500 shadow-lg"></div>
-                <!-- FULLNAME -->
-                <div>
-                    <p>Room ID: {{ room.id }}</p>
-                    <div class="flex items-center gap-2">
-                        <p class="text-gray-400">Preview mes... 00:00</p>
-                        <div class="size-3 rounded-full bg-green-400"></div>
-                    </div>
-                </div>
-            </div>
+            <Room v-for="room in room.list" :roomId="room.id" :latestMessage="room.latestMessage.content" @click="openRoom(room.id)" />
         </div>
     </div>
 </template>
