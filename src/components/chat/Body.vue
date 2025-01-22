085f1442c8f91ea9b7ebde0d256db04471bcaa13
Trần Gia Huy <tonytran0314@gmail.com>
2025-01-21 19:19:34 -0500

    feat: different color for my message and others

--- src/components/chat/Body.vue
+++ src/components/chat/Body.vue
@@ -41,7 +41,10 @@
     <div class="flex flex-col-reverse w-full flex-1 overflow-y-auto gap-0.5 px-2">
         <!-- CHAT LINE -->
         <!-- for the others text -->
-        <div v-for="message in messages" class="bg-gray-200 dark:bg-gray-700 px-4 py-2 max-w-[75%] rounded-2xl self-start">
+        <div v-for="message in messages" 
+            class=" px-4 py-2 max-w-[75%] rounded-full"
+            :class="(message.user_id === id) ? 'self-end bg-teal-600' : 'self-start bg-gray-200 dark:bg-gray-700'"
+        >
             {{ message.content }}
         </div> 
         <!-- for my text -->
