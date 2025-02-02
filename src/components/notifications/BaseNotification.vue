3893531ad3ef6cd58d0881e8618ac9c369a9bb80
Trần Gia Huy <tonytran0314@gmail.com>
2025-02-02 02:35:33 -0500

    style: friend request and base notification

---
+++ src/components/notifications/BaseNotification.vue
@@ -0,0 +1,9 @@
+<script setup>
+    
+</script>
+
+<template>
+    <div class="bg-gray-200 dark:bg-gray-700 py-3 px-4 rounded-lg">
+        <slot></slot>
+    </div>
+</template>
---
+++ src/components/notifications/FriendRequest.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
+    import SecondaryButton from '@/components/buttons/SecondaryButton.vue'
+    import BaseNotification from '@/components/notifications/BaseNotification.vue';
+
+    const props = defineProps({
+        fullname: {
+            type: String,
+            default: 'Gia Huy'
+        }
+    })
+</script>
+
+<template>
+    <BaseNotification class="space-y-4">
+        <p>{{ fullname }} has sent you a friend request</p>
+        <div class="space-x-2">
+            <PrimaryButton class="text-sm">Accept</PrimaryButton>
+            <SecondaryButton class="text-sm">Remove</SecondaryButton>
+        </div>
+    </BaseNotification>
+</template>
