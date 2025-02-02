bf934a2502e4392eac83032525bdc7fdf5219b8a
Trần Gia Huy <tonytran0314@gmail.com>
2025-02-02 03:11:31 -0500

    style: notification container

---
+++ src/components/modals/NotificationModal.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import FriendRequest from '@/components/notifications/FriendRequest.vue'
+</script>
+
+<template>
+    <!-- currently, there are 2 types of notifications: friend request and friend accept -->
+    <div class="h-96 space-y-3 overflow-auto">
+        <!-- notification items -->
+        <FriendRequest :fullname="'Trần Gia Huy'" />
+        <FriendRequest :fullname="'Gia Huy'" />
+    </div>
+</template>
