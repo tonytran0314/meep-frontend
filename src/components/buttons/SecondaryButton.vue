821069da8708b978c0bb4162639fdee7018f15cb
Trần Gia Huy <tonytran0314@gmail.com>
2025-02-02 02:16:16 -0500

    style: secondary button

--- src/components/buttons/SecondaryButton.vue
+++ src/components/buttons/SecondaryButton.vue
@@ -1,9 +1,16 @@
 <script setup>
-    
+    import BaseButton from '@/components/buttons/BaseButton.vue'
+
+    const props = defineProps({
+        buttonType: {
+            type: String,
+            default: 'button'
+        }
+    })
 </script>
 
 <template>
-    <div>
-        
-    </div>
+    <BaseButton :buttonType="buttonType" class="bg-gray-200 dark:bg-gray-500 text-white hover:bg-gray-100 hover:dark:bg-gray-400">
+        <slot></slot>
+    </BaseButton>
 </template>
