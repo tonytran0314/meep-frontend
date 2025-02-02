857b04dcf18070f026f6558909d977a67b375e1f
Trần Gia Huy <tonytran0314@gmail.com>
2025-02-02 02:34:29 -0500

    style: notification badge and click to open notification modal

--- src/components/rooms/Activities.vue
+++ src/components/rooms/Activities.vue
@@ -1,18 +1,26 @@
 <script setup>
     import { modalStore } from '@/stores/modal'
     import AddFriendModal from '@/components/modals/AddFriendModal.vue'
+    import NotificationModal from '@/components/modals/NotificationModal.vue'
 
     const modal = modalStore()
 
     const openAddFriendModal = () => {
         modal.open(AddFriendModal, { title: 'Add Friends' })
     }
+
+    const openNotificationModal = () => {
+        modal.open(NotificationModal, { title: 'Notifications' })
+    }
 </script>
 
 <template>
     <div class="flex justify-around">
         <font-awesome-icon :icon="['fas', 'user-plus']" @click="openAddFriendModal" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
-        <font-awesome-icon :icon="['fas', 'bell']" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
+        <div class="relative">
+            <font-awesome-icon :icon="['fas', 'bell']" @click="openNotificationModal" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
+            <span class="bg-red-600 rounded-full text-xs absolute -top-1.5 -right-1.5 size-5 flex items-center justify-center">9+</span>
+        </div>
         <font-awesome-icon :icon="['fas', 'comment-medical']" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
     </div>
 </template>
