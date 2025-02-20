05c2b07347fe46bf6aa9767280b0f3d968be1f8a
Huy Tran <tonytran0314@gmail.com>
2025-02-20 01:24:09 -0500

    feat: click icon to open new group modal

---
+++ src/components/modals/NewGroupModal.vue
@@ -0,0 +1,95 @@
+<script setup>
+    import { reactive } from 'vue'
+
+    import TextField from '@/components/form/TextField.vue'
+    import LoadingButton from '@/components/buttons/LoadingButton.vue'
+    import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
+
+    const newGroup = reactive({
+        name: null,
+        error: null, // temp
+        isLoading: false, // temp
+    })
+
+    const createGroup = () => {
+        console.log('created')
+    }
+</script>
+
+<template>
+    <form @submit.prevent="createGroup" class="space-y-4 md:space-y-6">
+        <TextField
+            v-model="newGroup.name"
+            label="Group name:"
+            type="text"
+            name="name"
+            placeholder="New group name..." 
+            :error="newGroup.error"
+        />
+
+        <div class="space-y-4">
+            <p>Friends list:</p>
+            <div class="h-72 overflow-auto space-y-4">
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'check']" class="cursor-pointer bg-teal-600 size-4 p-2 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+                <div class="flex justify-between">
+                    <p>User full name</p>
+                    <font-awesome-icon :icon="['fas', 'plus']" class="cursor-pointer bg-gray-200 dark:bg-gray-700 size-4 p-2 hover:bg-teal-600 dark:hover:bg-teal-600 flex justify-center items-center rounded-full" />
+                </div>
+            </div>
+        </div>
+
+        <LoadingButton v-if="newGroup.isLoading" class="w-full" />
+        <PrimaryButton v-else type="submit" class="w-full">Create</PrimaryButton>
+    </form>
+</template>
--- src/components/rooms/Activities.vue
+++ src/components/rooms/Activities.vue
@@ -1,11 +1,12 @@
 <script setup>
+    import { onMounted } from 'vue'
     import { modalStore } from '@/stores/modal'
     import { profileStore } from '@/stores/profile'
     import { notificationStore } from '@/stores/notification'
 
     import AddFriendModal from '@/components/modals/AddFriendModal.vue'
     import NotificationModal from '@/components/modals/NotificationModal.vue'
-import { onMounted } from 'vue'
+    import NewGroupModal from '@/components/modals/NewGroupModal.vue'
 
     const modal = modalStore()
     const notification = notificationStore()
@@ -18,6 +19,10 @@ import { onMounted } from 'vue'
     const openNotificationModal = () => {
         modal.open(NotificationModal, { title: 'Notifications' })
         notification.markAsSeen()
+    }
+
+    const openNewGroupModal = () => {
+        modal.open(NewGroupModal, { title: 'New Group Chat' })
     }
     
     onMounted(() => {
@@ -40,6 +45,6 @@ import { onMounted } from 'vue'
             <font-awesome-icon :icon="['fas', 'bell']" @click="openNotificationModal" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
             <span v-if="notification.list.length !== 0 || notification.newNotifcationIndicator" class="bg-red-400 rounded-full text-xs absolute top-1 right-1 size-2 flex items-center justify-center"></span>
         </div>
-        <font-awesome-icon :icon="['fas', 'comment-medical']" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
+        <font-awesome-icon @click="openNewGroupModal" :icon="['fas', 'comment-medical']" class="size-6 text-gray-700 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-3 rounded-full" />
     </div>
 </template>
