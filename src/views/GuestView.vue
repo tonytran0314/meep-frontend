f16ae26350bec48d228faa3247adc5af0665dd0d
Huy Tran <tonytran0314@gmail.com>
2025-02-24 13:54:14 -0500

    add login button to guest view

--- src/views/GuestView.vue
+++ src/views/GuestView.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-    <div class="bg-white dark:bg-gray-800 w-full h-screen flex justify-center items-center">
+    <div class="bg-white dark:bg-gray-800 w-full h-screen flex justify-center items-center flex-col gap-4">
         <p class="text-gray-900 dark:text-blue-50">guest vfiew</p>
+        <router-link to="/login" class="bg-teal-600 px-6 py-3 rounded hover:bg-teal-500">Login</router-link>
     </div>
 </template>
