55ddca42aa7766a37dd53aeb7bd606d197a5a390
Huy Tran <tonytran0314@gmail.com>
2025-03-09 23:02:26 -0400

    responsive layout for add friend modal

--- src/components/modals/CloseButton.vue
+++ src/components/modals/CloseButton.vue
@@ -6,7 +6,7 @@
 
 <template>
   <button @click="modal.close">
-    <font-awesome-icon :icon="['fas', 'xmark']" class="size-6 p-3 rounded-full text-gray-900 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700" />
+    <font-awesome-icon :icon="['fas', 'xmark']" class="size-4 sm:size-5 md:size-6 p-3 rounded-full text-gray-900 dark:text-blue-50 hover:bg-gray-200 dark:hover:bg-gray-700" />
   </button>
 </template>
   
--- src/components/rooms/SearchBar.vue
+++ src/components/rooms/SearchBar.vue
@@ -11,9 +11,10 @@
 </script>
 
 <template>
-    <form @submit.prevent="searchFriend" class="bg-white dark:bg-gray-700 border-2 dark:border-none px-4 py-2 rounded-full flex gap-3 items-center">   
-        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-gray-400" />
-        <input v-model="searchKeyword" type="text" class="bg-white dark:bg-gray-700 flex-1 focus:outline-none" placeholder="Search friends ...">
-        <button class="bg-teal-600 hover:bg-teal-500 px-3 rounded-full text-sm" type="submit">Search</button>
+    <form @submit.prevent="searchFriend" class="bg-white dark:bg-gray-700 border-2 dark:border-none pr-2 pl-4 py-2 rounded-full flex items-center">   
+        <!-- <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-gray-400" /> -->
+        <input v-model="searchKeyword" type="text" class="bg-white dark:bg-gray-700 flex-1 focus:outline-none text-sm md:text-md" placeholder="Search friends ...">
+        
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-white hover:bg-teal-600 p-2 rounded-full" />
     </form>
 </template>
