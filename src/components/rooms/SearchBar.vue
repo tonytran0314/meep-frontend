fe29b1d8120ba526f8266dccb9be50d1cfc094f5
Huy Tran <tonytran0314@gmail.com>
2025-03-09 23:05:02 -0400

    style: search friends button

--- src/components/rooms/SearchBar.vue
+++ src/components/rooms/SearchBar.vue
@@ -12,9 +12,8 @@
 
 <template>
     <form @submit.prevent="searchFriend" class="bg-white dark:bg-gray-700 border-2 dark:border-none pr-2 pl-4 py-2 rounded-full flex items-center">   
-        <!-- <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-gray-400" /> -->
-        <input v-model="searchKeyword" type="text" class="bg-white dark:bg-gray-700 flex-1 focus:outline-none text-sm md:text-md" placeholder="Search friends ...">
+        <input v-model="searchKeyword" type="text" class="bg-white dark:bg-gray-700 w-full focus:outline-none text-sm md:text-md" placeholder="Search friends ...">
         
-        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-white hover:bg-teal-600 p-2 rounded-full" />
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" class="text-white cursor-pointer bg-teal-600 hover:bg-teal-500 p-2 rounded-full" />
     </form>
 </template>
