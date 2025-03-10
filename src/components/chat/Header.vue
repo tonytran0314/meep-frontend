5ff7a555cd6cc00a6f862ee569057e8ebe529949
Huy Tran <tonytran0314@gmail.com>
2025-03-09 21:23:00 -0400

    feat: hide side bar on large screen

--- src/components/chat/Header.vue
+++ src/components/chat/Header.vue
@@ -37,7 +37,7 @@
     <div v-if="currentRoom" class="w-full h-12 flex justify-between items-center">
         <div class="flex items-center gap-4">
             <!-- SHOW SIDE BAR ICON -->
-            <font-awesome-icon @click="showSideBar" :icon="['fas', 'bars']" class="size-5 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-2 rounded-full" />
+            <font-awesome-icon @click="showSideBar" :icon="['fas', 'bars']" class="size-5 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer p-2 rounded-full lg:hidden" />
 
             <!-- ROOM INFO -->
             <div class="flex items-center gap-2">
